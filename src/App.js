79a1b1015960127f5bbff91758e640a12979ea53
cardozo <titocard2000@gmail.com>
2020-10-22 10:49:47 -0500

    segundo commit aprendiendo

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import './assets/css/App.css';
 
 import MiComponente from './component/MiComponente';
 
-function HelloWorld (name,edad){
+function HelloWorld (name,edad,est_civil,año){
   var presentacion=
     <div>
       <h2>Hola, soy {name}</h2>
       <h3>Tengo {edad} años</h3>
-    </div>
+      <h4> soy {est_civil}</h4>
+      <h4> naci en {año}</h4>
+    </div>;
   return presentacion;
 }
 
 function App() {
-  var name="Raul Cardozo";
+  var name="Raul Oscar Cardozo";
+  var est_civil="soltero";
   
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola!!!! Bienvenido Raul!!!
+          Hola, Bienvenido Raul Cardozo!!!
         </p>
-        {HelloWorld(name,45)}
-        <section className="componentes">
+        {HelloWorld(name,45, est_civil,1963)}
+        {/* {
+          alert("Argentina, Chaco, Resistencia")
+        }  
+        {
+           alert("Boludeando en react")
+        } */}
+          <section className="componentes">
             <MiComponente/>
         
         </section>  
